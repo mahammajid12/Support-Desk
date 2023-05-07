dd05be9af0f88b3ae79c67f4d617c4135cec0a40
mahammajid12 <i160186@nu.edu.pk>
2023-05-07 12:26:15 +0500

    Updating files

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('server...');
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+// connect to database
+connectDB()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/',(req,res)=>{
+    res.status(200).json({message: 'Welcome to the Support Desk API'})
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT,()=>{console.log(`Server started at PORT ${PORT}`)})
